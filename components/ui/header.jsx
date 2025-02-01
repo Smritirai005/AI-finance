365f1b0eff8b5d4da70fb5c2d9150bce97018c1c
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-02-01 21:09:17 +0530

    added components to the dashoboard

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -4,21 +4,26 @@ import React from 'react'
 import { SignInButton,SignedOut,SignedIn,UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+
 
 
 
 const Header = () => {
   return (
-    <div className='fixed top-0'>
-      <nav>
-        <Link href="/"><Image src={"/logo.png"} alt="fealth logo" height={60} width={200} className="h-12 w-auto object-contain"></Image></Link>
-      </nav>
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+      <nav className='container mx-auto px-4 py-4 flex items-center justify-between'> 
+        <Link href="/"><Image src={"/logo.png"} alt="fealth logo" height={60} width={400} className="h-12 w-auto object-contain"></Image></Link>
+      
         <SignedOut>
-            <SignInButton />
+          <SignInButton forceRedirectUrl='/dashboard'>
+          <Button variant="outline">Login</Button></SignInButton>
         </SignedOut>
         <SignedIn>
             <UserButton />
         </SignedIn>
+      </nav>
     </div>
   )
 }
